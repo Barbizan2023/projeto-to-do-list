3d3857e1e4e8bffb68cd48a8036d4a8f2a485212
Barbizan2023 <viniciusbarbizann@gmail.com>
2024-03-21 09:13:08 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import { View, TextInput } from "react-native";
+import Button from "./src/components/Button";
+import Task from "./src/components/Task";
+import styles from "./Global";
+
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTasks, setNewTasks] = useState("");
+
+  function addNewTask() {
+    setTasks([...tasks, {text: newTask, completed:false}]);
+  }
+
+  function ToggleTask(index) {
+    const newTasks = [...tasks];
+    newTasks[index].completed = !newTasks[index].completed;
+    setTasks(newTasks);
+  }
+
+  useEffect(() => {
+    const initialTasks = [
+      {text: "Atividade 1", completed: false},
+      {text: "Atividade 2", completed: true},
+      {text: "Atividade 3", completed: true},
+    ];
+    setTasks(initialTasks);
+  },{});
+  
+  return (
+    <View style={styles.container}>
+    <TextInput value={newTasks} 
+      style={styles.input}
+      onChangeText={(text) => setNewTasks(text)}/>
+    
+    <Button title="Adicionar" onPress={() => addNewTask()}/>
+    <View style={styles.listContainer}>
+      {tasks.map((task, index) => (
+        <Task key={task.text} isChecked={task.completed} label={task.text}
+         onPress={() => ToggleTask(index)}/>
+      ))}
+
+
+    </View>
+   </View>
+   )
+};
